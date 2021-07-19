96ddc2640e6b792b63bab331a32437ede2cfbc47
nelsonfvt <64501607+nelsonfvt@users.noreply.github.com>
2021-07-19 16:14:55 -0500

    Add files via upload

---
+++ Tarea_2/ejemplo_scr.m
@@ -0,0 +1,14 @@
+%señal exponencial
+n= 0:9;
+A=0.5;
+xn=A.^n;
+stem(n,xn)
+
+%Señal exponencial compleja
+n= 0:9;
+A=0.5+0.5*i;
+xn=A.^n;
+figure('Name','Parte real')
+stem(n,real(xn))
+figure('Name','Parte imaginaria')
+stem(n,imag(xn))
