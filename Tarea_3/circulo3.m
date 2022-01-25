fa785f55cc7b01926730a957b162a113ca31f5a0
nelsonfvt <64501607+nelsonfvt@users.noreply.github.com>
2022-01-24 19:44:03 -0500

    Delete circulo3.m

--- Tarea_3/circulo3.m
+++
@@ -1,9 +0,0 @@
-function circulo3(x,y,r)
-% Funcion para dibujar un circulo en coordenadas x,y de radio r
-
-ang=0:0.01:2*pi; 
-xp=r*cos(ang);
-yp=r*sin(ang);
-plot3(x+xp,y+yp,zeros(size(xp)))
-end
-
