428415c67ef78dfc356b9148bbd27773a73dd0ae
nelsonfvt <64501607+nelsonfvt@users.noreply.github.com>
2022-09-03 21:44:28 -0500

    Add files via upload

---
+++ Otros/Robot_model/Inv_model.py
@@ -0,0 +1,23 @@
+import sys
+import numpy as np
+import math
+
+EF = np.array([0.0, 0.0, 0.0, 1.0])
+
+for i in range(1, len(sys.argv)):
+    EF[i-1] = float(sys.argv[i])
+
+a = np.array([0.0, 10.0, 10.0])
+T = np.array([0.0, 0.0, 0.0])
+
+T[0] = np.arctan2(EF[1], EF[0])
+
+c3 = ( EF[0]**2 + EF[1]**2 + EF[2]**2 -a[1]**2 -a[2]**2) // (2 * a[1] * a[2])
+s3 = math.sqrt(1 - c3**2)
+T[2] = np.arctan2(s3, c3)
+
+s2 = ( (a[1]+a[2]*c3)*EF[2] - a[2]*s3*math.sqrt(EF[0]**2 + EF[1]**2) ) // (EF[0]**2 + EF[1]**2 + EF[2]**2)
+c2 = ( (a[1]+a[2]*c3)*math.sqrt(EF[0]**2 + EF[1]**2 + a[2]*s3*EF[2]) ) // (EF[0]**2 + EF[1]**2 + EF[2]**2)
+T[1] = np.arctan2(s2, c2)
+
+print(T)
