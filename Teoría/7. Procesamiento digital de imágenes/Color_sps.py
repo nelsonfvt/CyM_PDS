5387462f05b52a037dccc1c138af4fbff280183c
Nelson Velasco <nelsonfvt@gmail.com>
2023-03-04 14:16:01 -0500

    04/03/2023

--- Teoría/7. Procesamiento digital de imágenes/Color_sps.py
+++ Teoría/7. Procesamiento digital de imágenes/Color_sps.py
@@ -4,29 +4,44 @@ from matplotlib import pyplot as plt
 
 def RGB_HSV (imag):
     i_hsv = cv.cvtColor(imag, cv.COLOR_BGR2HSV)
-    cv.imshow('Original', imag)
-    cv.imshow('Imagen HSV', i_hsv)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+    plot_imags(imag, i_hsv)
+    
 
 def RGB_LAB(imag):
     i_lab = cv.cvtColor(imag, cv.COLOR_BGR2Lab)
-    cv.imshow('Original', imag)
-    cv.imshow('Imagen L*a*b*', i_lab)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+    plot_imags(imag, i_lab)
+    
 
-def plot_imgs(imag1, imag2):
-    plt.subplot(2,1,1)
-    plt.imshow(imag1, 'gray')
-    plt.subplot(2,1,2)
-    plt.imshow(imag2,'gray')
+def split_channs(imag):
+    C1, C2, C3 = cv.split(imag)
+    # plt.subplot(3,1,1)
+    # plt.imshow(C1, 'gray')
+    # plt.subplot(3,1,2)
+    # plt.imshow(C2, 'gray')
+    # plt.subplot(3,1,3)
+    # plt.imshow(C3, 'gray')
+    # plt.show()
+    
+def plot_imags(original, transformada):
+    plt.subplot(1,2,1)
+    plt.imshow(original, 'gray')
+    plt.subplot(1,2,2)
+    plt.imshow(transformada, 'gray')
+    plt.show()
 
-imagN = cv.imread('imag1.jpg') # CARGANDO IMAGEN NATURAL
-imagS = cv.imread('dibujo.png') # CARGANDO IMAGEN SINTÉTICA
+imagN = cv.imread('imag1.jpg') # CARGANDO IMAGEN NATURAL BGR
+imagS = cv.imread('dibujo.png') # CARGANDO IMAGEN SINTÉTICA BGR
 
-RGB_HSV(imagN)
-RGB_HSV(imagS)
+RGB_imagN = cv.cvtColor(imagN, cv.COLOR_BGR2RGB) #BGR -> RGB
+RGB_imagS = cv.cvtColor(imagS, cv.COLOR_BGR2RGB) #BGR -> RGB
 
-RGB_LAB(imagN)
-RGB_LAB(imagS)
+plot_imags(imagN, RGB_imagN)
+plot_imags(imagS, RGB_imagS)
+
+#imagen a HSV
+RGB_HSV(RGB_imagN)
+RGB_HSV(RGB_imagS)
+
+#imagen a L*a*b*
+RGB_LAB(RGB_imagN)
+RGB_LAB(RGB_imagS)
