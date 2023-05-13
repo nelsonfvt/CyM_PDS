8133d35afa8a4d761e661ad7dfce6110bc821e32
Nelson Velasco <nelsonfvt@gmail.com>
2023-05-13 13:26:05 -0500

    13 de Mayo 2023

--- Teoría/7. Procesamiento digital de imágenes/Esp_transf.py
+++ Teoría/7. Procesamiento digital de imágenes/Esp_transf.py
@@ -6,6 +6,8 @@ def rotate_imag(img, ang):
     rows,cols = img.shape
 
     M = cv.getRotationMatrix2D(((cols-1)/2.0, (rows-1)/2.0), ang, 1)
+    print('Matriz de transformación de rotación:')
+    print(M)
     dst = cv.warpAffine(img, M, (cols,rows))
 
     plot_imags(img,dst)
@@ -13,6 +15,8 @@ def rotate_imag(img, ang):
 def trans_imag(img, x ,y):
     rows,cols = img.shape
     M = np.float32([[1,0,x],[0,1,y]])
+    print('Matriz de transformación traslacion:')
+    print(M)
     dst = cv.warpAffine(img,M,(cols,rows))
     
     plot_imags(img,dst)
@@ -24,6 +28,8 @@ def affine_imag(img):
     pts2 = np.float32([[10,100],[200,50],[100,200]])
 
     M = cv.getAffineTransform(pts1,pts2)
+    print('Matriz de transformación Affine:')
+    print(M)
     dst = cv.warpAffine(img,M,(cols,rows))
     plot_imags(img,dst)
 
@@ -40,7 +46,7 @@ def plot_imags(img,dst):
 imagN = cv.imread('imag1.jpg', cv.IMREAD_GRAYSCALE) # CARGANDO IMAGEN NATURAL
 imagS = cv.imread('dibujo.png', cv.IMREAD_GRAYSCALE) # CARGANDO IMAGEN SINTÉTICA
 
-# Translación
+# Traslación
 trans_imag(imagN, 100, 50)
 trans_imag(imagS, 150, -100)
 
