93917d3b690256ecbdeb5ec3b7ac562d670270b5
Nelson Velasco <nelsonfvt@gmail.com>
2024-05-03 15:29:15 -0500

    modified:   "Teori\314\201a/6.Filtros de Kalman/Filtro de Kalman Extendido/CodigoSeales.py"

--- Teoría/6.Filtros de Kalman/Filtro de Kalman Extendido/CodigoSeales.py
+++ Teoría/6.Filtros de Kalman/Filtro de Kalman Extendido/CodigoSeales.py
@@ -41,20 +41,20 @@ R = np.eye(5) * 0.1
 P = np.eye(6)*0.1
 
 #Vector de estados
-estados = np.array([[X],
-                    [x_punto],
-                    [Y],
-                    [y_punto],
-                    [theta],
-                    [theta_punto]])
+estados = np.array([X,
+                    x_punto,
+                    Y,
+                    y_punto,
+                    theta,
+                    theta_punto])
 
 # Simulacion 10 segundos
 
-for i in range(1,1001):
+for i in range(0,1000):
     #atrasos de los estados
-    x_p_n1 = estados[1][0]
-    y_p_n1 = estados[3][0]
-    theta_p_n1 = estados[5][0]
+    x_p_n1 = estados[1]
+    y_p_n1 = estados[3]
+    theta_p_n1 = estados[5]
 
     # PREDICT
     # Calculo de velocidades del chasis
@@ -74,9 +74,9 @@ for i in range(1,1001):
     # Matriz F - Jacobiano
     
     F = np.array([[1, ts, 0, 0, 0, 0],
-                [0, 0, 0, 0, np.sin(estados[4][0])*(r/2)*(wr+wl), 0],
+                [0, 0, 0, 0, np.sin(estados[4])*(r/2)*(wr+wl), 0],
                 [0, 0, 1, ts, 0, 0],
-                [0, 0, 0, 0, np.cos(estados[4][0])*(r/2)*(wr+wl), 0],
+                [0, 0, 0, 0, np.cos(estados[4])*(r/2)*(wr+wl), 0],
                 [0, 0, 0, 0, 1, ts],
                 [0, 0, 0, 0, 0, 1]])
 
@@ -85,16 +85,48 @@ for i in range(1,1001):
 
     # UPDATE
     # Calculo hx
-    A_x = (estados[1][0] - x_p_n1) / ts
-    A_y = ri * (estados[5][0] - theta_p_n1) / ts
-    E_l = (np.sqrt(estados[1][0]**2 + estados[3][0]**2) - estados[5][0]*l) / r
-    E_r = (np.sqrt(estados[1][0]**2 + estados[3][0]**2) + estados[5][0]*l) / r
-    W_z = estados[5][0]
+    A_x = (estados[1] - x_p_n1) / ts
+    A_y = ri * (estados[5] - theta_p_n1) / ts
+    E_l = (np.sqrt(estados[1]**2 + estados[3]**2) - estados[5]*l) / r
+    E_r = (np.sqrt(estados[1]**2 + estados[3]**2) + estados[5]*l) / r
+    W_z = estados[5]
 
-    hx = [[A_x],
-        [A_y],
-        [E_l],
-        [E_r],
-        [W_z]]
+    hx = np.array([A_x,
+                A_y,
+                E_l,
+                E_r,
+                W_z])
 
-    print(hx)
+    #Comparacion con sensores - calculo y_m
+    #Capturar sensores
+    z = np.zeros(5)
+    y_m = z - hx
+
+    # Matriz H - Jacobiano
+    H = np.array([[0, 1.0/ts, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, ri/ts],
+                 [0, estados[1]/(np.sqrt(estados[1]**2 + estados[3]**2)*r), 0, -estados[3]/(np.sqrt(estados[1]**2 + estados[3]**2)*r), 0, l/r],
+                 [0, estados[1]/(np.sqrt(estados[1]**2 + estados[3]**2)*r), 0, -estados[3]/(np.sqrt(estados[1]**2 + estados[3]**2)*r), 0, -l/r],
+                 [0, 0, 0, 0, 0, 1]])
+
+    # Calculo de ganancia de Kalman
+    S = H.dot(P.dot(H.transpose())) + R
+    K = P.dot(H.transpose()).dot(np.linalg.inv(S))
+
+    # Ajuste de estados
+    estados = estados + K.dot(y_m)
+
+    # Ajsute Matriz P
+    P = (np.eye(6) - K.dot(H)).dot(P)
+
+    # Almacena datos
+    X_vec[i] = estados[0]
+    Y_vec[i] = estados[2]
+
+# Visualizacion de trayectoria
+plt.figure(1)
+plt.scatter(X_vec, Y_vec)
+plt.xlim(-2, 2)
+plt.ylim(-2, 2)
+plt.grid(1)
+plt.show()
