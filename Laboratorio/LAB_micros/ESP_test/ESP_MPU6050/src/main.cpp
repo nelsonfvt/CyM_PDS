a0db3431a44283bfe7130e7de66883d0533de214
Nelson Velasco <nelsonfvt@gmail.com>
2025-06-11 14:22:54 -0500

    ESP MPU6050 created

---
+++ Laboratorio/LAB_micros/ESP_test/ESP_MPU6050/src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
